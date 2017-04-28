f6e10c4f394e372bbfd95798434dd113e44c1cdb
Justin Kimn <djkimchee@gmail.com>
2017-04-27 23:11:11 -0700

    added logic for missing avatar image, just no default images loaded yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
+  has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true, default_url: "/assets/thumb/missing_avatar.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 
   has_many :posts, dependent: :destroy
   has_many :votes, dependent: :destroy
